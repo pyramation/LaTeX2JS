136f8e2eb1c1e015c0ed870f5b6e056a4967c2c9
Dan Lynch <pyramation@gmail.com>
2013-10-17 20:50:55 -0700

    added jquery plugin

---
+++ plugins/jquery.latex2html5.js
@@ -0,0 +1,29 @@
+// <latex src="some/path.tex"> 
+// will get replaced with the tex rendered
+$.fn.latex = function () {
+
+    $.each(this, function (i, el) {
+        var $el = $(el);
+        var url = $el.attr('src');
+        if (!url) {
+            throw new Error('!Error: latex element requires src attribute');
+        }
+        
+      jQuery.ajax({
+             url: url,
+             success: function(data) {
+
+                MathJax.Hub.Register.StartupHook("End",function () {
+                    var TEX = new LaTeX2HTML5.TeX({
+                        el: false,
+                        latex: data
+                    });
+                    TEX.render();
+                    $el.replaceWith(TEX.$el);
+                });
+             },
+             async:   true
+        });
+    });
+
+};
