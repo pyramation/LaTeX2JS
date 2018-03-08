79e92cbb78dea62d46032e52d15a947e60055137
Dan Lynch <pyramation@gmail.com>
2018-03-08 13:25:52 -0800

    moving to latex2js

--- plugins/latex2html5.image.js
+++
@@ -1,7 +0,0 @@
-// usage: \image{url}
-LaTeX2HTML5.addText(
-    'image',
-    /\\image\{[^}]*\}/g,            
-     LaTeX2HTML5.utils.matchrepl(/\\image\{([^}]*)\}/, function (m) {
-        return '<div style="width: 100%;text-align: center;"><img src="' + m[1] + '"></div>';
-    }));
