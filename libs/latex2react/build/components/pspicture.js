f58e4fd38ab6b02751831d047b3ee3b4a4aaaa90
Dan Lynch <pyramation@gmail.com>
2018-03-08 01:58:12 -0800

    updated latex2react

--- libs/latex2react/build/components/pspicture.js
+++ libs/latex2react/build/components/pspicture.js
@@ -3,7 +3,6 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.pspicture = undefined;
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -17,10 +16,6 @@ var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
-var _propTypes = require('prop-types');
-
-var _propTypes2 = _interopRequireDefault(_propTypes);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
@@ -31,23 +26,13 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var pspicture = exports.pspicture = function (_Component) {
+var pspicture = function (_Component) {
   _inherits(pspicture, _Component);
 
   function pspicture() {
-    var _ref;
-
-    var _temp, _this, _ret;
-
     _classCallCheck(this, pspicture);
 
-    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
-      args[_key] = arguments[_key];
-    }
-
-    return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = pspicture.__proto__ || Object.getPrototypeOf(pspicture)).call.apply(_ref, [this].concat(args))), _this), _this.state = {
-      // loaded: false,
-    }, _temp), _possibleConstructorReturn(_this, _ret);
+    return _possibleConstructorReturn(this, (pspicture.__proto__ || Object.getPrototypeOf(pspicture)).apply(this, arguments));
   }
 
   _createClass(pspicture, [{
@@ -66,7 +51,6 @@ var pspicture = exports.pspicture = function (_Component) {
       var size = _latex2jsPstricks.psgraph.getSize.call(this.props);
       var width = size.width + 'px';
       var height = size.height + 'px';
-
       return _react2.default.createElement(
         'div',
         {
@@ -89,3 +73,5 @@ var pspicture = exports.pspicture = function (_Component) {
 
   return pspicture;
 }(_react.Component);
+
+exports.default = pspicture;
--- libs/latex2react/src/components/pspicture.js
+++ libs/latex2react/src/components/pspicture.js
@@ -1,25 +1,18 @@
 import { psgraph } from 'latex2js-pstricks';
 import * as d3 from 'd3';
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 
-export class pspicture extends Component {
-  state = {
-    // loaded: false,
-  };
-
+export default class pspicture extends Component {
   componentDidMount() {
     var d3svg = d3.select(this.refSvg);
     const obj = { ...this.props };
     obj.$el = this.refDiv;
     psgraph.pspicture.call(obj, d3svg);
   }
-
   render() {
     const size = psgraph.getSize.call(this.props);
     const width = `${size.width}px`;
     const height = `${size.height}px`;
-
     return (
       <div
         className="pspicture"
